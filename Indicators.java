51eca61d0a9682d52d191d69e2ba90ae5ae15daf
Kevin Zhang <kwz5@pitt.edu>
2014-09-28 00:15:24 -0400

    Added SMA

--- Indicators.java
+++ Indicators.java
@@ -10,4 +10,24 @@ package technicalindicators;
  * @author Kevin
  */
 public class Indicators {
+    private double lowThreshold;
+    private double highThreshold;
+    private double currentValue;
+    public int dateLine;
+
+
+    public void setCurrentValue(double currentValue) {
+        this.currentValue = currentValue;
+    }
+
+    public void setHighThreshold(double highThreshold) {
+        this.highThreshold = highThreshold;
+    }
+
+    public void setLowThreshold(double lowThreshold) {
+        this.lowThreshold = lowThreshold;
+    }
+    
+
+    
 }
---
+++ SMA.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package technicalindicators;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Kevin
+ */
+public class SMA extends Indicators {
+
+    ArrayList<Double> SMASet = new ArrayList<Double>();
+
+    public SMA() {
+        super();
+    }
+    
+}
